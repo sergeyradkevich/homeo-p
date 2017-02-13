d4722e54f2db39c8f520ea7bdbee449b4d01b0fb
sergiiradkevych <sergiiradkevych>
2017-02-14 01:42:06 +0200

    introduce a test double in form of in-memory gateway for getting prescribed drugs

---
+++ src/main/java/entities/Drug.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class Drug implements Cloneable {
+    private String name;
+
+    public Drug(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
--- src/test/java/GetPrescribedDrugsUseCaseTest.java
+++ src/test/java/GetPrescribedDrugsUseCaseTest.java
@@ -1,23 +1,80 @@
+import entities.Drug;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class GetPrescribedDrugsUseCaseTest {
 
+    private Gateway gateway = new Gateway();
+    private GetPrescribedDrugsUseCase useCase;
+
+    @Before
+    public void setUp() {
+        useCase = new GetPrescribedDrugsUseCase(gateway);
+    }
+
     @Test
-    public void givenNoDrugsReturnsEmpty() {
-        GetPrescribedDrugsUseCase useCase = new GetPrescribedDrugsUseCase();
-        List<String> drugs = useCase.getPrescribedDrugs();
+    public void givenNoDrugsReturnsNoneDrugs() {
+        List<Drug> drugs = useCase.getPrescribedDrugs();
         assertTrue(drugs.isEmpty());
+    }
+
+
+    @Test
+    public void givenOnePrescribedDrugReturnsOneDrug() {
+        Drug drug = new Drug("Arsen Alb");
+        gateway.save(drug);
+
+        List<Drug> drugs = useCase.getPrescribedDrugs();
+
+        assertEquals("Arsen Alb", drugs.get(0).getName());
+        assertEquals(1, drugs.size());
+    }
+
+}
+
+class Gateway {
+
+    private Set<Drug> entities = new HashSet<>();
+
+    public Drug save(Drug drug) {
+        persist(drug);
+        return drug;
+    }
+
+    public List<Drug> findAll() {
+        return entities.stream()
+                .collect(Collectors.toList());
+    }
+
+    private void persist(Drug drug) {
+        try {
+            entities.add((Drug) drug.clone());
+        }
+        catch (CloneNotSupportedException e) {
+            throw new UnCloneable();
+        }
     }
 }
 
 class GetPrescribedDrugsUseCase {
-    public List<String> getPrescribedDrugs() {
-      return Collections.emptyList();
+    private Gateway gateway;
+
+    public GetPrescribedDrugsUseCase(Gateway gateway) {
+        this.gateway = gateway;
+    }
+
+    public List<Drug> getPrescribedDrugs() {
+        return gateway.findAll();
     }
 }
+
+class UnCloneable extends RuntimeException {}
 
