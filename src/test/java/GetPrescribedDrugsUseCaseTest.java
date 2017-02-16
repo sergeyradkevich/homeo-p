ac9370d73f58013798eff69c48a967864ab7808e
sergiiradkevych <sergiiradkevych>
2017-02-17 00:21:29 +0200

    make in-memory gateway to behave like db with no effects on the data it holds

--- src/test/java/GetPrescribedDrugsUseCaseTest.java
+++ src/test/java/GetPrescribedDrugsUseCaseTest.java
@@ -2,10 +2,9 @@ import entities.Drug;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -26,7 +25,6 @@ public class GetPrescribedDrugsUseCaseTest {
         assertTrue(drugs.isEmpty());
     }
 
-
     @Test
     public void givenOnePrescribedDrugReturnsOneDrug() {
         Drug drug = new Drug("Arsen Alb");
@@ -36,6 +34,45 @@ public class GetPrescribedDrugsUseCaseTest {
 
         assertEquals("Arsen Alb", drugs.get(0).getName());
         assertEquals(1, drugs.size());
+    }
+
+    //todo: introduce equality based on identity, not names
+    @Test
+    public void givenManyPrescribedDrugsReturnsThem() {
+        String[] names = {"Arsen Alb", "Arsen Alb 1", "Arsen Alb 2"};
+
+        Stream.of(names)
+                .map(name -> (new Drug(name)))
+                .forEach(drug -> gateway.save(drug));
+
+        List<Drug> drugs = useCase.getPrescribedDrugs();
+
+        List<String> persistedNames = drugs.stream()
+                .map(drug -> drug.getName())
+                .sorted()
+                .collect(Collectors.toList());
+        assertEquals(Arrays.asList(names), persistedNames);
+    }
+
+    @Test
+    public void ImMemoryGatewayBehavesLikeDb_updateOfSavedEntityHasNoEffectOnDataInMemory() {
+        Drug drug = new Drug("Arsen Alb");
+        gateway.save(drug);
+
+        drug.setName("Arsen Alb +++");
+        Drug persisted = gateway.findAll().get(0);
+
+        assertEquals("Arsen Alb", persisted.getName());
+    }
+
+    @Test
+    public void ImMemoryGatewayBehavesLikeDb_updateOfRetrievedEntityHasNoEffectOnDataInMemory() {
+        gateway.save(new Drug("Arsen Alb"));
+
+        Drug persisted = gateway.findAll().get(0);
+        persisted.setName("Arsen Alb ---");
+
+        assertEquals("Arsen Alb", gateway.findAll().get(0).getName());
     }
 
 }
@@ -51,17 +88,25 @@ class Gateway {
 
     public List<Drug> findAll() {
         return entities.stream()
+                .map(e -> clone(e))
                 .collect(Collectors.toList());
     }
 
     private void persist(Drug drug) {
+       Drug clone = clone(drug);
+       entities.add(clone);
+    }
+
+    private Drug clone(Drug drug) {
         try {
-            entities.add((Drug) drug.clone());
+            return (Drug) drug.clone();
         }
         catch (CloneNotSupportedException e) {
             throw new UnCloneable();
         }
     }
+
+
 }
 
 class GetPrescribedDrugsUseCase {
